afd27cb50757a121c93a4ef009b3289ae2135e4d
Manyata Roka <161047309+JuniorManyata@users.noreply.github.com>
2026-05-07 15:56:09 +0545

    login view

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,181 +1,181 @@
-// import 'package:flutter/material.dart';
-// import 'package:handmade_crafts/views/signup_view.dart';
-// import 'package:handmade_crafts/views/dashboard_view.dart';
+import 'package:flutter/material.dart';
+import 'package:handmade_crafts/views/signup_view.dart';
+import 'package:handmade_crafts/views/dashboard_view.dart';
 
-// class LoginView extends StatefulWidget {
-//   const LoginView({super.key});
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
 
-//   @override
-//   State<LoginView> createState() => _LoginViewState();
-// }
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
 
-// class _LoginViewState extends State<LoginView> {
-//   bool isPasswordVisible = false;
-//   bool rememberMe = false;
+class _LoginViewState extends State<LoginView> {
+  bool isPasswordVisible = false;
+  bool rememberMe = false;
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Container(
-//         width: double.infinity,
-//         decoration: const BoxDecoration(
-//           gradient: LinearGradient(
-//             colors: [Color(0xFFF5F5F5), Color(0xFFF8D7DA)],
-//             begin: Alignment.topCenter,
-//             end: Alignment.bottomCenter,
-//           ),
-//         ),
-//         child: SafeArea(
-//           child: Padding(
-//             padding: const EdgeInsets.symmetric(horizontal: 20),
-//             child: Column(
-//               children: [
-//                 const SizedBox(height: 40),
-//                 Image.asset('assets/images/picture1.png', height: 100),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFFF5F5F5), Color(0xFFF8D7DA)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                const SizedBox(height: 40),
+                Image.asset('assets/images/picture1.png', height: 100),
 
-//                 const SizedBox(height: 20),
-//                 Column(
-//                   children: [
-//                     const Text(
-//                       "Login",
-//                       style: TextStyle(
-//                         fontSize: 22,
-//                         fontWeight: FontWeight.w500,
-//                         color: Colors.grey,
-//                       ),
-//                     ),
-//                     const SizedBox(height: 5),
-//                     Container(height: 3, width: 60, color: Colors.red),
-//                   ],
-//                 ),
+                const SizedBox(height: 20),
+                Column(
+                  children: [
+                    const Text(
+                      "Login",
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    Container(height: 3, width: 60, color: Colors.red),
+                  ],
+                ),
 
-//                 const SizedBox(height: 30),
-//                 TextField(
-//                   decoration: InputDecoration(
-//                     hintText: "Email Address",
-//                     prefixIcon: const Icon(Icons.email_outlined),
-//                     filled: true,
-//                     fillColor: Colors.white.withOpacity(0.8),
-//                     border: OutlineInputBorder(
-//                       borderRadius: BorderRadius.circular(10),
-//                       borderSide: BorderSide.none,
-//                     ),
-//                   ),
-//                 ),
+                const SizedBox(height: 30),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: "Email Address",
+                    prefixIcon: const Icon(Icons.email_outlined),
+                    filled: true,
+                    fillColor: Colors.white.withOpacity(0.8),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
 
-//                 const SizedBox(height: 15),
-//                 TextField(
-//                   obscureText: !isPasswordVisible,
-//                   decoration: InputDecoration(
-//                     hintText: "Password",
-//                     prefixIcon: const Icon(Icons.lock_outline),
-//                     suffixIcon: IconButton(
-//                       icon: Icon(
-//                         isPasswordVisible
-//                             ? Icons.visibility
-//                             : Icons.visibility_off,
-//                       ),
-//                       onPressed: () {
-//                         setState(() {
-//                           isPasswordVisible = !isPasswordVisible;
-//                         });
-//                       },
-//                     ),
-//                     filled: true,
-//                     fillColor: Colors.white.withOpacity(0.8),
-//                     border: OutlineInputBorder(
-//                       borderRadius: BorderRadius.circular(10),
-//                       borderSide: BorderSide.none,
-//                     ),
-//                   ),
-//                 ),
+                const SizedBox(height: 15),
+                TextField(
+                  obscureText: !isPasswordVisible,
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    prefixIcon: const Icon(Icons.lock_outline),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        isPasswordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          isPasswordVisible = !isPasswordVisible;
+                        });
+                      },
+                    ),
+                    filled: true,
+                    fillColor: Colors.white.withOpacity(0.8),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
 
-//                 const SizedBox(height: 10),
-//                 Row(
-//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                   children: [
-//                     Row(
-//                       children: [
-//                         Checkbox(
-//                           value: rememberMe,
-//                           onChanged: (value) {
-//                             setState(() {
-//                               rememberMe = value!;
-//                             });
-//                           },
-//                         ),
-//                         const Text("Remember me"),
-//                       ],
-//                     ),
-//                     TextButton(
-//                       onPressed: () {},
-//                       child: const Text(
-//                         "Forgot password?",
-//                         style: TextStyle(color: Colors.blueGrey),
-//                       ),
-//                     ),
-//                   ],
-//                 ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Checkbox(
+                          value: rememberMe,
+                          onChanged: (value) {
+                            setState(() {
+                              rememberMe = value!;
+                            });
+                          },
+                        ),
+                        const Text("Remember me"),
+                      ],
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Forgot password?",
+                        style: TextStyle(color: Colors.blueGrey),
+                      ),
+                    ),
+                  ],
+                ),
 
-//                 const SizedBox(height: 10),
-//                 SizedBox(
-//                   width: double.infinity,
-//                   height: 50,
-//                   child: ElevatedButton(
-//                     style: ElevatedButton.styleFrom(
-//                       backgroundColor: Colors.redAccent,
-//                       shape: RoundedRectangleBorder(
-//                         borderRadius: BorderRadius.circular(10),
-//                       ),
-//                     ),
-//                     onPressed: () {
-//                       Navigator.pushAndRemoveUntil(
-//                         context,
-//                         MaterialPageRoute(
-//                           builder: (_) => const DashboardView(),
-//                         ),
-//                         (route) => false,
-//                       );
-//                     },
-//                     child: const Text(
-//                       "LOGIN",
-//                       style: TextStyle(letterSpacing: 1, fontSize: 16),
-//                     ),
-//                   ),
-//                 ),
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.redAccent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => const DashboardView(),
+                        ),
+                        (route) => false,
+                      );
+                    },
+                    child: const Text(
+                      "LOGIN",
+                      style: TextStyle(letterSpacing: 1, fontSize: 16),
+                    ),
+                  ),
+                ),
 
-//                 const SizedBox(height: 20),
-//                 Row(
-//                   mainAxisAlignment: MainAxisAlignment.center,
-//                   children: [
-//                     const Text(
-//                       "Don’t have an account? ",
-//                       style: TextStyle(color: Colors.grey),
-//                     ),
-//                     GestureDetector(
-//                       onTap: () {
-//                         Navigator.push(
-//                           context,
-//                           MaterialPageRoute(
-//                             builder: (context) => const SignupView(),
-//                           ),
-//                         );
-//                       },
-//                       child: const Text(
-//                         "Register",
-//                         style: TextStyle(
-//                           color: Colors.redAccent,
-//                           fontWeight: FontWeight.bold,
-//                         ),
-//                       ),
-//                     ),
-//                   ],
-//                 ),
-//               ],
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Don’t have an account? ",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const SignupView(),
+                          ),
+                        );
+                      },
+                      child: const Text(
+                        "Register",
+                        style: TextStyle(
+                          color: Colors.redAccent,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
