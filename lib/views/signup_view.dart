cf2d973dba626f15f8fedea12ab52cfbc06fa5e3
Manyata Roka <161047309+JuniorManyata@users.noreply.github.com>
2026-05-29 15:46:47 +0545

    signup view updated

--- lib/views/signup_view.dart
+++ lib/views/signup_view.dart
@@ -1,278 +1,278 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class SignupView extends StatefulWidget {
-//   const SignupView({super.key});
+class SignupView extends StatefulWidget {
+  const SignupView({super.key});
 
-//   @override
-//   State<SignupView> createState() => _SignupViewState();
-// }
+  @override
+  State<SignupView> createState() => _SignupViewState();
+}
 
-// class _SignupViewState extends State<SignupView> {
-//   bool isPasswordVisible = false;
+class _SignupViewState extends State<SignupView> {
+  bool isPasswordVisible = false;
 
-//   final TextEditingController usernameCtrl =
-//       TextEditingController();
+  final TextEditingController usernameCtrl =
+      TextEditingController();
 
-//   final TextEditingController emailCtrl =
-//       TextEditingController();
+  final TextEditingController emailCtrl =
+      TextEditingController();
 
-//   final TextEditingController passwordCtrl =
-//       TextEditingController();
+  final TextEditingController passwordCtrl =
+      TextEditingController();
 
-//   @override
-//   void dispose() {
-//     usernameCtrl.dispose();
-//     emailCtrl.dispose();
-//     passwordCtrl.dispose();
-//     super.dispose();
-//   }
+  @override
+  void dispose() {
+    usernameCtrl.dispose();
+    emailCtrl.dispose();
+    passwordCtrl.dispose();
+    super.dispose();
+  }
 
-//   void register() {
-//     ScaffoldMessenger.of(context).showSnackBar(
-//       const SnackBar(
-//         content: Text("Registered successfully"),
-//       ),
-//     );
+  void register() {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text("Registered successfully"),
+      ),
+    );
 
-//     Navigator.pop(context);
-//   }
+    Navigator.pop(context);
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Container(
-//         width: double.infinity,
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
 
-//         decoration: const BoxDecoration(
-//           gradient: LinearGradient(
-//             colors: [
-//               Color(0xFFF5F5F5),
-//               Color(0xFFF8D7DA),
-//             ],
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFFF5F5F5),
+              Color(0xFFF8D7DA),
+            ],
 
-//             begin: Alignment.topCenter,
-//             end: Alignment.bottomCenter,
-//           ),
-//         ),
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
 
-//         child: SafeArea(
-//           child: SingleChildScrollView(
-//             child: SizedBox(
-//               height: MediaQuery.of(context).size.height,
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height,
 
-//               child: Padding(
-//                 padding:
-//                     const EdgeInsets.symmetric(horizontal: 20),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20),
 
-//                 child: Column(
-//                   mainAxisAlignment:
-//                       MainAxisAlignment.center,
+                child: Column(
+                  mainAxisAlignment:
+                      MainAxisAlignment.center,
 
-//                   children: [
-//                     /// LOGO
-//                     Image.asset(
-//                       'assets/images/picture1.png',
-//                       height: 100,
-//                     ),
+                  children: [
+                    /// LOGO
+                    Image.asset(
+                      'assets/images/picture1.png',
+                      height: 100,
+                    ),
 
-//                     const SizedBox(height: 20),
+                    const SizedBox(height: 20),
 
-//                     /// TITLE
-//                     Column(
-//                       children: [
-//                         const Text(
-//                           "Register",
+                    /// TITLE
+                    Column(
+                      children: [
+                        const Text(
+                          "Register",
 
-//                           style: TextStyle(
-//                             fontSize: 22,
-//                             fontWeight: FontWeight.w500,
-//                             color: Colors.grey,
-//                           ),
-//                         ),
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.grey,
+                          ),
+                        ),
 
-//                         const SizedBox(height: 5),
+                        const SizedBox(height: 5),
 
-//                         Container(
-//                           height: 3,
-//                           width: 70,
-//                           color: Colors.red,
-//                         ),
-//                       ],
-//                     ),
+                        Container(
+                          height: 3,
+                          width: 70,
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
 
-//                     const SizedBox(height: 30),
+                    const SizedBox(height: 30),
 
-//                     /// USERNAME
-//                     TextField(
-//                       controller: usernameCtrl,
+                    /// USERNAME
+                    TextField(
+                      controller: usernameCtrl,
 
-//                       decoration: InputDecoration(
-//                         hintText: "Username",
+                      decoration: InputDecoration(
+                        hintText: "Username",
 
-//                         prefixIcon: const Icon(
-//                           Icons.person_outline,
-//                         ),
+                        prefixIcon: const Icon(
+                          Icons.person_outline,
+                        ),
 
-//                         filled: true,
+                        filled: true,
 
-//                         fillColor:
-//                             Colors.white.withOpacity(0.8),
+                        fillColor:
+                            Colors.white.withOpacity(0.8),
 
-//                         border: OutlineInputBorder(
-//                           borderRadius:
-//                               BorderRadius.circular(10),
+                        border: OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.circular(10),
 
-//                           borderSide: BorderSide.none,
-//                         ),
-//                       ),
-//                     ),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
 
-//                     const SizedBox(height: 15),
+                    const SizedBox(height: 15),
 
-//                     /// EMAIL
-//                     TextField(
-//                       controller: emailCtrl,
+                    /// EMAIL
+                    TextField(
+                      controller: emailCtrl,
 
-//                       keyboardType:
-//                           TextInputType.emailAddress,
+                      keyboardType:
+                          TextInputType.emailAddress,
 
-//                       decoration: InputDecoration(
-//                         hintText: "Email Address",
+                      decoration: InputDecoration(
+                        hintText: "Email Address",
 
-//                         prefixIcon: const Icon(
-//                           Icons.email_outlined,
-//                         ),
+                        prefixIcon: const Icon(
+                          Icons.email_outlined,
+                        ),
 
-//                         filled: true,
+                        filled: true,
 
-//                         fillColor:
-//                             Colors.white.withOpacity(0.8),
+                        fillColor:
+                            Colors.white.withOpacity(0.8),
 
-//                         border: OutlineInputBorder(
-//                           borderRadius:
-//                               BorderRadius.circular(10),
+                        border: OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.circular(10),
 
-//                           borderSide: BorderSide.none,
-//                         ),
-//                       ),
-//                     ),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
 
-//                     const SizedBox(height: 15),
+                    const SizedBox(height: 15),
 
-//                     /// PASSWORD
-//                     TextField(
-//                       controller: passwordCtrl,
+                    /// PASSWORD
+                    TextField(
+                      controller: passwordCtrl,
 
-//                       obscureText: !isPasswordVisible,
+                      obscureText: !isPasswordVisible,
 
-//                       decoration: InputDecoration(
-//                         hintText: "Password",
+                      decoration: InputDecoration(
+                        hintText: "Password",
 
-//                         prefixIcon: const Icon(
-//                           Icons.lock_outline,
-//                         ),
+                        prefixIcon: const Icon(
+                          Icons.lock_outline,
+                        ),
 
-//                         suffixIcon: IconButton(
-//                           icon: Icon(
-//                             isPasswordVisible
-//                                 ? Icons.visibility
-//                                 : Icons.visibility_off,
-//                           ),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            isPasswordVisible
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                          ),
 
-//                           onPressed: () {
-//                             setState(() {
-//                               isPasswordVisible =
-//                                   !isPasswordVisible;
-//                             });
-//                           },
-//                         ),
+                          onPressed: () {
+                            setState(() {
+                              isPasswordVisible =
+                                  !isPasswordVisible;
+                            });
+                          },
+                        ),
 
-//                         filled: true,
+                        filled: true,
 
-//                         fillColor:
-//                             Colors.white.withOpacity(0.8),
+                        fillColor:
+                            Colors.white.withOpacity(0.8),
 
-//                         border: OutlineInputBorder(
-//                           borderRadius:
-//                               BorderRadius.circular(10),
+                        border: OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.circular(10),
 
-//                           borderSide: BorderSide.none,
-//                         ),
-//                       ),
-//                     ),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
 
-//                     const SizedBox(height: 20),
+                    const SizedBox(height: 20),
 
-//                     /// REGISTER BUTTON
-//                     SizedBox(
-//                       width: double.infinity,
-//                       height: 50,
+                    /// REGISTER BUTTON
+                    SizedBox(
+                      width: double.infinity,
+                      height: 50,
 
-//                       child: ElevatedButton(
-//                         style: ElevatedButton.styleFrom(
-//                           backgroundColor:
-//                               Colors.redAccent,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              Colors.redAccent,
 
-//                           shape:
-//                               RoundedRectangleBorder(
-//                             borderRadius:
-//                                 BorderRadius.circular(10),
-//                           ),
-//                         ),
+                          shape:
+                              RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.circular(10),
+                          ),
+                        ),
 
-//                         onPressed: register,
+                        onPressed: register,
 
-//                         child: const Text(
-//                           "REGISTER",
+                        child: const Text(
+                          "REGISTER",
 
-//                           style: TextStyle(
-//                             letterSpacing: 1,
-//                             fontSize: 16,
-//                             color: Colors.white,
-//                           ),
-//                         ),
-//                       ),
-//                     ),
+                          style: TextStyle(
+                            letterSpacing: 1,
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
 
-//                     const SizedBox(height: 20),
+                    const SizedBox(height: 20),
 
-//                     /// LOGIN NAVIGATION
-//                     Row(
-//                       mainAxisAlignment:
-//                           MainAxisAlignment.center,
+                    /// LOGIN NAVIGATION
+                    Row(
+                      mainAxisAlignment:
+                          MainAxisAlignment.center,
 
-//                       children: [
-//                         const Text(
-//                           "Already have an account? ",
+                      children: [
+                        const Text(
+                          "Already have an account? ",
 
-//                           style: TextStyle(
-//                             color: Colors.grey,
-//                           ),
-//                         ),
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
 
-//                         GestureDetector(
-//                           onTap: () {
-//                             Navigator.pop(context);
-//                           },
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
 
-//                           child: const Text(
-//                             "Sign in",
+                          child: const Text(
+                            "Sign in",
 
-//                             style: TextStyle(
-//                               color: Colors.blueGrey,
-//                               fontWeight: FontWeight.bold,
-//                             ),
-//                           ),
-//                         ),
-//                       ],
-//                     ),
-//                   ],
-//                 ),
-//               ),
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+                            style: TextStyle(
+                              color: Colors.blueGrey,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
