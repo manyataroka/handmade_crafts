86871a4d86b80c06c995ada852ee21e09d2b5d93
Manyata Roka <161047309+JuniorManyata@users.noreply.github.com>
2026-06-05 14:19:06 +0545

    progress more

--- lib/core/data/database/local_hive_database.dart
+++ lib/core/data/database/local_hive_database.dart
@@ -1,30 +1,30 @@
-import 'package:hive_flutter/hive_flutter.dart';
+// import 'package:hive_flutter/hive_flutter.dart';
 
-class LocalHiveDatabase {
-  static const String boxName = 'localBox';
+// class LocalHiveDatabase {
+//   static const String boxName = 'localBox';
 
-  static Future<void> init() async {
-    await Hive.initFlutter();
-    if (!Hive.isBoxOpen(boxName)) {
-      await Hive.openBox(boxName);
-    }
-  }
+//   static Future<void> init() async {
+//     await Hive.initFlutter();
+//     if (!Hive.isBoxOpen(boxName)) {
+//       await Hive.openBox(boxName);
+//     }
+//   }
 
-  static Box get box => Hive.box(boxName);
+//   static Box get box => Hive.box(boxName);
 
-  static Future<void> put(String key, dynamic value) async {
-    await box.put(key, value);
-  }
+//   static Future<void> put(String key, dynamic value) async {
+//     await box.put(key, value);
+//   }
 
-  static dynamic get(String key, {dynamic defaultValue}) {
-    return box.get(key, defaultValue: defaultValue);
-  }
+//   static dynamic get(String key, {dynamic defaultValue}) {
+//     return box.get(key, defaultValue: defaultValue);
+//   }
 
-  static Future<void> delete(String key) async {
-    await box.delete(key);
-  }
+//   static Future<void> delete(String key) async {
+//     await box.delete(key);
+//   }
 
-  static Future<void> clear() async {
-    await box.clear();
-  }
-}
+//   static Future<void> clear() async {
+//     await box.clear();
+//   }
+// }
