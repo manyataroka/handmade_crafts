06ea00e670f9d2874a8a5b43179408ce96595e5e
Manyata Roka <161047309+JuniorManyata@users.noreply.github.com>
2026-07-29 21:30:17 +0545

    routes

--- lib/features/onboarding/domain/entities/onboarding_item.dart
+++ lib/features/onboarding/domain/entities/onboarding_item.dart
@@ -1,17 +1,17 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class OnboardingItem {
-//   final String title;
-//   final String description;
-//   final IconData icon;
-//   final Color color;
-//   final List<Color> gradientColors;
+class OnboardingItem {
+  final String title;
+  final String description;
+  final IconData icon;
+  final Color color;
+  final List<Color> gradientColors;
 
-//   const OnboardingItem({
-//     required this.title,
-//     required this.description,
-//     required this.icon,
-//     required this.color,
-//     required this.gradientColors,
-//   });
-// }
+  const OnboardingItem({
+    required this.title,
+    required this.description,
+    required this.icon,
+    required this.color,
+    required this.gradientColors,
+  });
+}
