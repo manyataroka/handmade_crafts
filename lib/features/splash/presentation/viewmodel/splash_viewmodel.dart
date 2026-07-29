d4fc93165d35acf39793be0619aad45710fba809
Manyata Roka <161047309+JuniorManyata@users.noreply.github.com>
2026-07-29 21:31:51 +0545

    splash view

--- lib/features/splash/presentation/viewmodel/splash_viewmodel.dart
+++ lib/features/splash/presentation/viewmodel/splash_viewmodel.dart
@@ -1,14 +1,14 @@
-import 'dart:async';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'dart:async';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-enum SplashState { initial, loading, completed }
+// enum SplashState { initial, loading, completed }
 
-class SplashViewModel extends StateNotifier<SplashState> {
-  SplashViewModel() : super(SplashState.initial);
+// class SplashViewModel extends StateNotifier<SplashState> {
+//   SplashViewModel() : super(SplashState.initial);
 
-  Future<void> startSplash() async {
-    state = SplashState.loading;
-    await Future.delayed(const Duration(seconds: 2));
-    state = SplashState.completed;
-  }
-}
+//   Future<void> startSplash() async {
+//     state = SplashState.loading;
+//     await Future.delayed(const Duration(seconds: 2));
+//     state = SplashState.completed;
+//   }
+// }
