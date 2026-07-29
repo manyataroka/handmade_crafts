4c6025e44c81700118a476cdbb4ed0457dea81e9
Manyata Roka <161047309+JuniorManyata@users.noreply.github.com>
2026-07-29 08:34:41 +0545

    Update

--- lib/features/onboarding/presentation/viewmodel/onboarding_viewmodel.dart
+++ lib/features/onboarding/presentation/viewmodel/onboarding_viewmodel.dart
@@ -1,22 +1,22 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class OnboardingViewModel extends StateNotifier<int> {
-  OnboardingViewModel() : super(0);
+// class OnboardingViewModel extends StateNotifier<int> {
+//   OnboardingViewModel() : super(0);
 
-  int get currentPage => state;
-  int totalPages = 3;
+//   int get currentPage => state;
+//   int totalPages = 3;
 
-  void setPage(int page) {
-    if (page >= 0 && page < totalPages) {
-      state = page;
-    }
-  }
+//   void setPage(int page) {
+//     if (page >= 0 && page < totalPages) {
+//       state = page;
+//     }
+//   }
 
-  void nextPage() {
-    if (state < totalPages - 1) {
-      state++;
-    }
-  }
+//   void nextPage() {
+//     if (state < totalPages - 1) {
+//       state++;
+//     }
+//   }
 
-  bool get isLastPage => state == totalPages - 1;
-}
+//   bool get isLastPage => state == totalPages - 1;
+// }
